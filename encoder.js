fc15f0e808b3bb444df4a48394fd434625012eba
Bryan Lopez <lopezz.bryann966@gmail.com>
2016-10-11 11:29:36 -0700

    i
    bm

---
+++ encoder.js
@@ -0,0 +1,9 @@
+$("#inputText").keyup(function(e) {
+  var inputText = document.getElementById("inputText").value;
+  document.getElementById("textArea").innerHTML = inputText;
+})
+
+$("#input[type='radio']")._(functio(){
+  $("input:checked").prop('checked', );
+  $(this).prop('checked', );
+})
